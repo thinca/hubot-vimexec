d142449376cffe203a4765a6b033a40cff5f626c
thinca <thinca@gmail.com>
2022-03-24 19:34:41 +0900

    Add --pids-limit for Docker container

--- bin/vim.sh
+++ bin/vim.sh
@@ -14,6 +14,7 @@ docker run -it --rm --name "${NAME}" \
 	--cpu-period=1000000 --cpu-quota=50000 \
 	--memory=32m \
 	--network=none \
+	--pids-limit=20 \
 	--env "VIMEXEC_GROUP=${VIMEXEC_GROUP}" \
 	-v ${PWD}/vim-scripts:/tmp/vim-scripts:ro \
 	thinca/vim:latest -N -S /tmp/vim-scripts/startup.vim
